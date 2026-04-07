d5648aa017c4f2976b2f76864d866068d9d4ef4f
ujeong <qnf323@naver.com>
2026-04-07 20:14:43 +0900

    test: 포인트 사용 시 포인트가 차감된다

--- src/test/kotlin/domain/UserTest.kt
+++ src/test/kotlin/domain/UserTest.kt
@@ -5,9 +5,16 @@ import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
 import org.junit.jupiter.api.assertThrows
 
-class User(val id: Long, val point: Long) {
+class User(val id: Long) {
+    var point: Long = 1000
+
     init {
         require(id > 0) { "ID는 양수이어야 합니다." }
+    }
+
+    fun discountPoint(discount: Long) {
+        require(point - discount > 0) { "차감액은 전체 포인트보다 작아야 합니다." }
+        point -= discount
     }
 }
 
@@ -15,7 +22,7 @@ class UserTest {
     @Test
     fun `ID는 양수일 때 정상 생성 되어야 한다`() {
         // given & when : User의 ID에 양수값을 넣는다.
-        val result = User(1, 1000)
+        val result = User(1)
 
         // then : 정상 생성 된다.
         assertNotNull(result)
@@ -25,10 +32,22 @@ class UserTest {
     fun `ID가 음수일 때 예외가 발생한다`() {
         // given & when : User의 ID에 음수값을 넣는다.
         val exception = assertThrows<IllegalArgumentException> {
-            User(-1, 1000)
+            User(-1)
         }
 
         // then : 예외가 발생한다.
         assertEquals(exception.message, "ID는 양수이어야 합니다.")
+    }
+
+    @Test
+    fun `포인트 사용 시 포인트가 차감된다`() {
+        // given : User 객체가 주어진다
+        val result = User(1)
+
+        // when : discountPoint로 500을 입력했을 때
+        result.discountPoint(500)
+
+        // then : 전체 포인트 값이 500이 된다.
+        assertEquals(result.point, 500)
     }
 }
