ceb5364e938da4818689d4f1f7b5610827826ecb
ujeong <qnf323@naver.com>
2026-04-07 19:34:21 +0900

    test: S석의 가격은 18,000원이다

---
+++ src/test/kotlin/domain/SeatGradeTest.kt
@@ -0,0 +1,14 @@
+package domain
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class SeatGradeTest {
+    @Test
+    fun `S석의 가격은 18,000원이다`() {
+        // given & when : SeatGrade에서 S석을 생성할 때
+        val result = SeatGrade.S
+        // then : 해당 자리의 가격은 18_000이다.
+        assertEquals(result.price, 18_000)
+    }
+}
