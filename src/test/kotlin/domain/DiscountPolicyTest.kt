309e2648264e07726b886e4763a0752d302cd7d2
ujeong <qnf323@naver.com>
2026-04-07 20:36:33 +0900

    test: 무비데이가 아닌 날에는 무비데이 할인이 적용되지 않는다

--- src/test/kotlin/domain/DiscountPolicyTest.kt
+++ src/test/kotlin/domain/DiscountPolicyTest.kt
@@ -6,12 +6,13 @@ import org.junit.jupiter.api.Test
 object DiscountPolicy {
     private val movieDay = listOf<Int>(10, 20, 30)
     const val MOVIE_DAY_DISCOUNT_PERCENT = 0.1
+    const val NONE_DISCOUNT_PERCENT = 0.0
 
     fun movieDayDiscount(date: Int): Double {
         if (movieDay.contains(date)) {
             return MOVIE_DAY_DISCOUNT_PERCENT
         }
-        return 0.0
+        return NONE_DISCOUNT_PERCENT
     }
 }
 
@@ -50,5 +51,17 @@ class DiscountPolicyTest {
 
         // then : 0.1이 반환된다.
         assertEquals(result, DiscountPolicy.MOVIE_DAY_DISCOUNT_PERCENT)
+    }
+
+    @Test
+    fun `무비데이가 아닌 날에는 무비데이 할인이 적용되지 않는다`() {
+        // given : 날짜가 1일이다.
+        val date = 1
+
+        // when : 할인율을 계산하면
+        val result = DiscountPolicy.movieDayDiscount(date)
+
+        // then : 0.0이 반환된다.
+        assertEquals(result, DiscountPolicy.NONE_DISCOUNT_PERCENT)
     }
 }
