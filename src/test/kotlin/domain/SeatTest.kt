1a8f6f48387eda1e0aca149af4ad822948e9e12c
ujeong <qnf323@naver.com>
2026-04-07 19:24:03 +0900

    test: 열이 한 글자 대문자 알파벳이 아니면 예외가 발생한다

--- src/test/kotlin/domain/SeatTest.kt
+++ src/test/kotlin/domain/SeatTest.kt
@@ -1,7 +1,9 @@
 package domain
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertThrows
 
 class Seat(row: Char, column: Int, grade: SeatGrade, isReserved: Boolean) {
     init {
@@ -22,5 +24,16 @@ class SeatTest {
         val result = Seat('A', 1, SeatGrade.S, false)
         // then : 정상 생성된다.
         assertNotNull(result)
+    }
+
+    @Test
+    fun `열이 한 글자 대문자 알파벳이 아니면 예외가 발생한다`() {
+        // given & when : Seat 객체를 생성할 때 한 글자 소문자 알파벳을 입력한다.
+        val exception = assertThrows<IllegalArgumentException> {
+            Seat('a', 1, SeatGrade.S, false)
+        }
+
+        // then : 예외가 발생한다.
+        assertEquals(exception.message, "열은 한 글자 대문자 알파벳이여야 합니다.")
     }
 }
