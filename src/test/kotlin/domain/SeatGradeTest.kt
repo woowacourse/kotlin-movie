56a1dd4f7fa78395839a834213467ea07898aaf2
ujeong <qnf323@naver.com>
2026-04-07 19:35:23 +0900

    test: A석의 가격은 15,000원이다

--- src/test/kotlin/domain/SeatGradeTest.kt
+++ src/test/kotlin/domain/SeatGradeTest.kt
@@ -11,4 +11,12 @@ class SeatGradeTest {
         // then : 해당 자리의 가격은 18_000이다.
         assertEquals(result.price, 18_000)
     }
+
+    @Test
+    fun `A석의 가격은 15,000원이다`() {
+        // given & when : SeatGrade에서 A석을 생성할 때
+        val result = SeatGrade.A
+        // then : 해당 자리의 가격은 15_000이다.
+        assertEquals(result.price, 15_000)
+    }
 }
