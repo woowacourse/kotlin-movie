ae63c27276d4aa7c301f45fbd7545d90e760eb11
ujeong <qnf323@naver.com>
2026-04-07 19:50:40 +0900

    test: ID가 음수일 때 예외가 발생한다

--- src/test/kotlin/domain/UserTest.kt
+++ src/test/kotlin/domain/UserTest.kt
@@ -1,7 +1,9 @@
 package domain
 
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertNotNull
+import org.junit.jupiter.api.assertThrows
 
 class User(val id: Long, val point: Long) {
     init {
@@ -17,5 +19,16 @@ class UserTest {
 
         // then : 정상 생성 된다.
         assertNotNull(result)
+    }
+
+    @Test
+    fun `ID가 음수일 때 예외가 발생한다`() {
+        // given & when : User의 ID에 음수값을 넣는다.
+        val exception = assertThrows<IllegalArgumentException> {
+            User(-1, 1000)
+        }
+
+        // then : 예외가 발생한다.
+        assertEquals(exception.message, "ID는 양수이어야 합니다.")
     }
 }
