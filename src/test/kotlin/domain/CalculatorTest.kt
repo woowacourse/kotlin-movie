826ec2fe938dbabef4060dd238632f8a4d61872e
ujeong <qnf323@naver.com>
2026-04-08 15:25:41 +0900

    chore: 테스트 객체를 프로덕션 코드로 이동

--- src/test/kotlin/domain/CalculatorTest.kt
+++ src/test/kotlin/domain/CalculatorTest.kt
@@ -4,49 +4,6 @@ import kotlinx.datetime.LocalDateTime
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
-object Calculator {
-
-    fun subtractUserPoint(
-        price: Int,
-        user: User,
-        subtractPoint: Long,
-    ): Long {
-        user.discountPoint(subtractPoint)
-        return price - subtractPoint
-    }
-
-    fun calculateByMovie(
-        price: Int,
-        date: LocalDateTime,
-    ): Int {
-        var discountedPrice = applyMovieDayDiscount(price, date)
-        discountedPrice = applyTimeDiscount(discountedPrice, date)
-
-        return discountedPrice
-    }
-
-    fun applyMovieDayDiscount(
-        price: Int,
-        date: LocalDateTime,
-    ): Int {
-        return ((1 - DiscountPolicy.movieDayDiscount(date)) * price).toInt()
-    }
-
-    fun applyTimeDiscount(
-        price: Int,
-        date: LocalDateTime,
-    ): Int {
-        return price - DiscountPolicy.showTimeDiscount(date)
-    }
-
-    fun applyPaymentDiscount(
-        price: Int,
-        method: PaymentMethod,
-    ): Int {
-        return ((1 - DiscountPolicy.paymentDiscount(method)) * price).toInt()
-    }
-}
-
 class CalculatorTest {
     @Test
     fun `무비데이에 상영되는 영화는 10% 할인이 적용된다`() {
