c20a3bded6547d1130f6a350c45931861fd4c3d4
baekcii <bsy8470@naver.com>
2026-04-08 16:57:10 +0900

    feat: 좌석 예약 상태 추가

--- src/main/kotlin/domain/Seat.kt
+++ src/main/kotlin/domain/Seat.kt
@@ -3,7 +3,13 @@ package domain
 data class Seat(
     val row: Row,
     val column: Column,
+    val state: ReserveState = ReserveState.AVAILABLE,
 ) {
     val grade: SeatGrade
         get() = SeatGrade.of(row = row, column = column)
+}
+
+enum class ReserveState {
+    RESERVED,
+    AVAILABLE,
 }
