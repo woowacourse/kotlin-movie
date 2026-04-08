b7175b0312f83be9268460c0080c87e2056657df
baekcii <bsy8470@naver.com>
2026-04-08 18:27:46 +0900

    test: Screening 테스트 코드 작성 및 기능 목록 업데이트

---
+++ src/test/kotlin/domain/ScreeningTest.kt
@@ -0,0 +1,73 @@
+package domain
+
+import org.junit.jupiter.api.Assertions.assertThrows
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
+import javax.swing.text.Position
+
+class ScreeningTest {
+    @Test
+    fun `영화, 상영관, 상영 날짜 및 시간을 가진다`() {
+        Screening(
+            movie = Movie(
+                title = Title("허닛"),
+                runningTime = RunningTime(167),
+                screeningPeriod = ScreeningPeriod(
+                    startDate = LocalDate.of(2026, 4, 8),
+                    endDate = LocalDate.of(2026, 4, 9)
+                )
+            ),
+            room = ScreeningRoom(
+                name = ScreeningRoomName("커피"),
+                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                seats = Seats(listOf(Seat(position = SeatPosition(Row("A"), Column(1))))),            ),
+            startTime = LocalDateTime.of(2026, 4, 8, 10, 0)
+        )
+    }
+
+    @Test
+    fun `상영 시간이 상영관의 운영시간에 포함되지 않을 경우 예외를 던진다`() {
+        assertThrows(IllegalArgumentException::class.java) {
+            Screening(
+                movie = Movie(
+                    title = Title("허닛"),
+                    runningTime = RunningTime(167),
+                    screeningPeriod = ScreeningPeriod(
+                        startDate = LocalDate.of(2026, 4, 8),
+                        endDate = LocalDate.of(2026, 4, 9)
+                    )
+                ),
+                room = ScreeningRoom(
+                    name = ScreeningRoomName("커피"),
+                    operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                    seats = Seats(listOf(Seat(position = SeatPosition(Row("A"), Column(1))))),                ),
+                startTime = LocalDateTime.of(2026, 4, 8, 9, 0)
+            )
+        }
+    }
+
+    @Test
+    fun `좌석을 예약 시 좌석의 상태가 바뀐다`() {
+        val screening = Screening(
+            movie = Movie(
+                title = Title("허닛"),
+                runningTime = RunningTime(167),
+                screeningPeriod = ScreeningPeriod(
+                    startDate = LocalDate.of(2026, 4, 8),
+                    endDate = LocalDate.of(2026, 4, 9)
+                )
+            ),
+            room = ScreeningRoom(
+                name = ScreeningRoomName("커피"),
+                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                seats = Seats(listOf(Seat(position = SeatPosition(Row("A"), Column(1))))),
+            ),
+            startTime = LocalDateTime.of(2026, 4, 8, 9, 0)
+        )
+
+        screening.reserve(position = SeatPosition(Row("A"), Column(1)))
+    }
+}
