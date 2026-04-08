2e6e8474339f5862ddab707206dc2c056fb0cd65
ujeong <qnf323@naver.com>
2026-04-08 14:36:49 +0900

    test: 무비데이에 상영되는 영화는 10% 할인이 적용된다

---
+++ src/test/kotlin/domain/CalculatorTest.kt
@@ -0,0 +1,38 @@
+package domain
+
+import kotlinx.datetime.LocalDateTime
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+object Calculator {
+    fun applyDiscountByMovie(
+        price: Int,
+        date: LocalDateTime,
+    ): Int {
+        val discountedPrice = applyMovieDayDiscount(price, date)
+
+        return discountedPrice
+    }
+
+    fun applyMovieDayDiscount(
+        price: Int,
+        date: LocalDateTime,
+    ): Int {
+        return ((1 - DiscountPolicy.movieDayDiscount(date)) * price).toInt()
+    }
+}
+
+class CalculatorTest {
+    @Test
+    fun `무비데이에 상영되는 영화는 10% 할인이 적용된다`() {
+        // given : 날짜가 10일이고, 영화가 16_000원이다.
+        val price = 16_000
+        val date = LocalDateTime(2026, 4, 10, 14, 0)
+
+        // when : 할인율을 적용하면
+        val result = Calculator.applyMovieDayDiscount(price, date)
+
+        // then : 14_400원이 반환된다.
+        assertEquals(14_400, result)
+    }
+}
