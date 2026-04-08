dcd3e4f641c8fc2eeddbdb5e4577899f96d5b56e
byunghyunkim0 <bb05qudgus@gmail.com>
2026-04-08 18:18:58 +0900

    test: Movie Title test 코드 작성
    - Title domain 구현

---
+++ src/main/kotlin/domain/movie/itmes/Title.kt
@@ -0,0 +1,12 @@
+package domain.movie.itmes
+
+@JvmInline
+value class Title(
+    private val title: String,
+) {
+    init {
+        require(title.isNotBlank()) { "타이틀은 비어있을 수 없습니다. (입력값: $title)" }
+    }
+
+    fun isSame(title: String): Boolean = this.title == title
+}
---
+++ src/test/kotlin/domain/movie/items/TitleTest.kt
@@ -0,0 +1,31 @@
+package domain.movie.items
+
+import domain.movie.itmes.Title
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.api.assertThrows
+
+class TitleTest {
+    @Test
+    fun `영화의 이름은 빈 문자열이 예외를 던진다`() {
+        assertThrows<IllegalArgumentException> { Title("") }
+    }
+
+    @Test
+    fun `입력받은 영화 제목이 Title과 같으면 True를 반환한다`() {
+        val title = Title("신바드의 모험")
+
+        val result = title.isSame("신바드의 모험")
+
+        assertThat(result).isTrue()
+    }
+
+    @Test
+    fun `입력받은 영화 제목이 Title과 같지 않으면 false를 반환한다`() {
+        val title = Title("신바드의 모험")
+
+        val result = title.isSame("신밧드의 모험")
+
+        assertThat(result).isFalse()
+    }
+}
