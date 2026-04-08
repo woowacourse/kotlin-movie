e91e6b661ab706a6ce2addb0cc7539eaeabfc726
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-09 05:07:24 +0900

    feat: 영화(Movie) 객체 및 테스트 추가

---
+++ src/main/kotlin/domain/movie/Movie.kt
@@ -0,0 +1,9 @@
+package domain.movie
+
+import domain.screening.Screenings
+import java.util.UUID
+
+data class Movie(
+    val id: UUID,
+    val screenings: Screenings
+)
---
+++ src/test/kotlin/domain/movie/MovieTest.kt
@@ -0,0 +1,22 @@
+package domain.movie
+
+import domain.screening.Screenings
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+import java.util.UUID
+
+class MovieTest {
+
+    @Test
+    fun `영화 ID와 상영 목록을 갖고 있다`() {
+        // given
+        val id = UUID.randomUUID()
+        val screenings = Screenings(emptyList())
+        val movie = Movie(id, screenings)
+        // when
+
+        // then
+        assertThat(id).isEqualTo(id)
+        assertThat(movie.screenings).isEqualTo(screenings)
+    }
+}
