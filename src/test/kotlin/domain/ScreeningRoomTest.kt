b7a4f4b9ed5a5bb532da310786e98d9bb3ba789c
baekcii <bsy8470@naver.com>
2026-04-08 17:58:26 +0900

    refactor: 상영관 기능 구현 체크 및 테스트 코드 수정

--- src/test/kotlin/domain/ScreeningRoomTest.kt
+++ src/test/kotlin/domain/ScreeningRoomTest.kt
@@ -8,10 +8,9 @@ class ScreeningRoomTest {
     @Test
     fun `이름, 운영시간, 좌석 목록을 가진다`() {
         ScreeningRoom(
-            name = "챱",
-            startTime = LocalTime.of(10, 0),
-            endTime = LocalTime.of(18, 0),
-            seats = listOf(Seat(Row("A"), Column(1))),
+            name = ScreeningRoomName("커피"),
+            operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+            seats = Seats(listOf(Seat(Row("A"), Column(1)))),
         )
     }
 
@@ -19,10 +18,9 @@ class ScreeningRoomTest {
     fun `이름이 공백일 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
             ScreeningRoom(
-                name = " ",
-                startTime = LocalTime.of(10, 0),
-                endTime = LocalTime.of(18, 0),
-                seats = listOf(Seat(Row("A"), Column(1))),
+                name = ScreeningRoomName(" "),
+                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                seats = Seats(listOf(Seat(Row("A"), Column(1)))),
             )
         }
     }
@@ -31,10 +29,9 @@ class ScreeningRoomTest {
     fun `운영 종료 시간이 시작 시간보다 앞설 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
             ScreeningRoom(
-                name = " 커비",
-                startTime = LocalTime.of(10, 0),
-                endTime = LocalTime.of(9, 0),
-                seats = listOf(Seat(Row("A"), Column(1))),
+                name = ScreeningRoomName("커피"),
+                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(9, 0)),
+                seats = Seats(listOf(Seat(Row("A"), Column(1)))),
             )
         }
     }
@@ -43,10 +40,9 @@ class ScreeningRoomTest {
     fun `좌석이 없을 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
             ScreeningRoom(
-                name = " 커비",
-                startTime = LocalTime.of(10, 0),
-                endTime = LocalTime.of(9, 0),
-                seats = emptyList(),
+                name = ScreeningRoomName("커피"),
+                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                seats = Seats(emptyList()),
             )
         }
     }
