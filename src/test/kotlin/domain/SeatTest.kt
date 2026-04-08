4c9c002ccec109b8f2855acd4201d6b3990b229d
baekcii <bsy8470@naver.com>
2026-04-08 16:22:57 +0900

    test: Seat객체 변경사항 반영

--- src/test/kotlin/domain/SeatTest.kt
+++ src/test/kotlin/domain/SeatTest.kt
@@ -6,27 +6,27 @@ import org.junit.jupiter.api.Test
 class SeatTest {
     @Test
     fun `행과 열을 가진다`() {
-        Seat(row = "A", column = 1)
+        Seat(row = Row("A"), column = Column(1))
     }
 
     @Test
     fun `행이 A~J 범위가 아닐 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
-            Seat(row = "K", column = 1)
+            Seat(row = Row("K"), column = Column(1))
         }
     }
 
     @Test
     fun `행이 알파벳이 아닌 문자가 포함될 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
-            Seat(row = "Cㅓ비", column = 1)
+            Seat(row = Row("Cㅓ비"), column = Column(1))
         }
     }
 
     @Test
     fun `열이 1~12 범위가 아닐 경우 예외를 던진다`() {
         assertThrows(IllegalArgumentException::class.java) {
-            Seat(row = "J", column = 13)
+            Seat(row = Row("J"), column = Column(13))
         }
     }
 }
