ac4f7ea1057ac5bc0f966dc04ecf755ce074dc61
byunghyunkim0 <bb05qudgus@gmail.com>
2026-04-08 19:02:06 +0900

    test: Movie test 코드 작성
    - Movie 도메인 구현

---
+++ src/main/kotlin/domain/movie/Movie.kt
@@ -0,0 +1,16 @@
+package domain.movie
+
+import domain.movie.itmes.RunningTime
+import domain.movie.itmes.ScreeningPeriod
+import domain.movie.itmes.Title
+import java.time.LocalDate
+
+class Movie(
+    private val title: Title,
+    private val runningTime: RunningTime,
+    private val screeningPeriod: ScreeningPeriod,
+) {
+    fun isValidTitle(title: String): Boolean = this.title.isSame(title)
+
+    fun isScreening(date: LocalDate): Boolean = this.screeningPeriod.isContain(date)
+}
