6f37c6f76e53c075b1155cfa8d58d6b96cce9e31
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-08 16:44:22 +0900

    fix: 좌석목록 객체 추가에 따른 Sceen 객체 수정

--- src/main/kotlin/domain/screening/Screen.kt
+++ src/main/kotlin/domain/screening/Screen.kt
@@ -1,15 +1,16 @@
 package domain.screening
 
 import domain.seat.Seat
+import domain.seat.SeatGrade
+import domain.seat.Seats
 import java.util.UUID
 
 class Screen(
     val id: Int,
-    val seats: List<Seat>,
+    val seats: Seats
 ){
     init{
         require(id > 0) { "상영관 ID는 0보다 커야 합니다." }
-        require(seats.isNotEmpty()) { "좌석 목록은 비어 있을 수 없습니다." }
     }
 }
 
--- src/test/kotlin/domain/screening/ScreenTest.kt
+++ src/test/kotlin/domain/screening/ScreenTest.kt
@@ -1,7 +1,6 @@
 package domain.screening
 
-import domain.seat.Seat
-import domain.seat.SeatGrade
+import domain.seat.Seats
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.assertThrows
 
@@ -10,29 +9,17 @@ class ScreenTest {
     @Test
     fun `상영관은 상영관ID와 좌석 목록을 갖는다`(){
 
-        val screen = Screen(1, listOf(
-            Seat("A", 1, SeatGrade.B),
-            Seat("A", 2, SeatGrade.B),
-            Seat("A", 3, SeatGrade.B),
-            Seat("A", 4, SeatGrade.B),
-        ))
+        val screen = Screen(1, seats  = Seats.createDefault())
 
         assert(screen.id == 1)
-        assert(screen.seats.size == 4)
+        assert(screen.seats.size() ==20)
     }
 
     @Test
     fun `상영관 번호는 0보다 커야 한다`(){
         val exception = assertThrows<IllegalArgumentException> {
-            Screen(0, listOf())
+            Screen(0, Seats.createDefault())
         }
         assert(exception.message == "상영관 ID는 0보다 커야 합니다.")
-    }
-    @Test
-    fun `상영관 좌석 목록은 비어있을 수 없다`(){
-        val exception = assertThrows<IllegalArgumentException> {
-            Screen(1, listOf())
-        }
-        assert(exception.message == "좌석 목록은 비어 있을 수 없습니다.")
     }
 }
