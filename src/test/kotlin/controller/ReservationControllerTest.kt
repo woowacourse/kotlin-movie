beaf60beb5d541aaaadcec9199ebac974bfa8aa3
ujeong <qnf323@naver.com>
2026-04-08 20:07:13 +0900

    test: 생성 여부가 Y일 경우 true를 반환한다

--- src/test/kotlin/controller/ReservationControllerTest.kt
+++ src/test/kotlin/controller/ReservationControllerTest.kt
@@ -9,11 +9,16 @@ class ReservationController {
     fun run() {
     }
 
-    fun create() {
+    fun create(): Boolean {
         println("영화 예매를 시작합니다. 새 예매를 생성하시겠습니까? (Y/N)")
         val input = readln()
 
         require(input == "Y" || input == "N") { "입력값은 Y 혹은 N이어야 합니다." }
+
+        when (input) {
+            "Y" -> return true
+        }
+        return false
     }
 }
 
@@ -34,5 +39,18 @@ class ReservationControllerTest {
 
         // then : 예외가 발생한다.
         assertEquals("입력값은 Y 혹은 N이어야 합니다.", exception.message)
+    }
+
+    @Test
+    fun `생성 여부가 Y일 경우 true를 반환한다`() {
+        // given : 생성 여부가 Y로 입력된다.
+        val input = "Y"
+        System.setIn(ByteArrayInputStream(input.toByteArray()))
+
+        // when : 입력 여부를 처리하면
+        val result = controller.create()
+
+        // then : 예외가 발생한다.
+        assertEquals(true, result)
     }
 }
