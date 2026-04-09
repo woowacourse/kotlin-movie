cb8596f91aa63bd23bff7eebcdac2f315685bb29
baekcii <bsy8470@naver.com>
2026-04-09 12:28:37 +0900

    feat: Money 클래스 산술 연산자 구현

--- src/main/kotlin/domain/Money.kt
+++ src/main/kotlin/domain/Money.kt
@@ -4,4 +4,9 @@ data class Money(val amount: Int) {
     init {
         require(amount >= 0) { "돈은 음수일 수 없습니다." }
     }
+    operator fun plus(other: Money): Money = Money(this.amount + other.amount)
+    operator fun plus(other: Int): Money = Money(this.amount + other)
+
+    operator fun minus(other: Money): Money = Money(this.amount - other.amount)
+    operator fun minus(other: Int): Money = Money(this.amount - other)
 }
