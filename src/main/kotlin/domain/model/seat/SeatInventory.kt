ad9e9c6954d593f9d67e22e2b5d391e2937e4691
parkhyomi <phyomi3@naver.com>
2026-04-09 16:45:10 +0900

    feat: SeatInventory 도메인 모델 추가

---
+++ src/main/kotlin/domain/model/seat/SeatInventory.kt
@@ -0,0 +1,43 @@
+package domain.model.seat
+
+import domain.Seat
+
+// 상영 1건의 전체 좌석 상태를 관리한다.
+data class SeatInventory(
+    private val seats: List<SeatAvailability>,
+) {
+    // 원하는 좌석을 받았을 때 -> 예약이 되는지 안되는지
+    fun isAvailable(targetSeat: Seat): Boolean =
+        seats.any { seatAvailability ->
+            seatAvailability.isSeat(targetSeat) && seatAvailability.isAvailable()
+        }
+
+    // 예약 상태를 변경
+    fun reserve(targetSeat: Seat): SeatInventory {
+        require(isAvailable(targetSeat)) { "이미 예약된 좌석입니다." }
+        return copy(
+            seats =
+                seats.map { seatAvailability ->
+                    if (!seatAvailability.isSeat(targetSeat)) {
+                        return@map seatAvailability
+                    }
+                    seatAvailability.reserve()
+                },
+        )
+    }
+
+    // 현재 좌석 상태 목록을 반환한다.
+    fun statuses(): List<SeatAvailability> = seats
+
+    companion object {
+        // 스크린당 좌석의 초기세팅
+        fun defaultSeatAvailabilities(): List<SeatAvailability> =
+            Seat.columns.flatMap { row ->
+                Seat.rows.map { column ->
+                    SeatAvailability(
+                        seat = Seat(row, column),
+                    )
+                }
+            }
+    }
+}
