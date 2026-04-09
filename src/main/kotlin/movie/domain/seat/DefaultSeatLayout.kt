abab626f18563a43e4a070f078259673ecaccd59
chohs4164 <josh100as@gmail.com>
2026-04-09 17:25:30 +0900

    refactor: 레이아웃 랭크 수정

--- src/main/kotlin/movie/domain/seat/DefaultSeatLayout.kt
+++ src/main/kotlin/movie/domain/seat/DefaultSeatLayout.kt
@@ -4,11 +4,13 @@ import movie.domain.seat.number.Column
 import movie.domain.seat.number.Row
 import movie.domain.seat.number.SeatNumber
 import movie.domain.seat.rank.ARank
+import movie.domain.seat.rank.BRank
+import movie.domain.seat.rank.SRank
 
 class DefaultSeatLayout : SeatLayout {
     override fun createSeats(): List<Seat> {
-        val rows = 'A'..'F'
-        val cols = 1..5
+        val rows = 'A'..'E'
+        val cols = 1..4
 
         return rows.flatMap { row ->
             cols.map { col ->
@@ -18,7 +20,12 @@ class DefaultSeatLayout : SeatLayout {
                             row = Row(row),
                             col = Column(col),
                         ),
-                    rank = ARank(),
+                    rank = if (row in 'A'..'B')
+                        BRank()
+                    else if (row in 'C'..'D')
+                        SRank()
+                    else
+                        ARank(),
                 )
             }
         }
