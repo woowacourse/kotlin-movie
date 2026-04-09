e7d21fd3aa8a558aba6689171fed3136d00da3e2
baekcii <bsy8470@naver.com>
2026-04-09 17:09:37 +0900

    refactor: 메인에서 상영 스케줄 주입받도록 수정

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,58 @@
 import controller.Controller
+import domain.Column
+import domain.Movie
+import domain.Row
+import domain.RunningTime
+import domain.Screening
+import domain.ScreeningPeriod
+import domain.ScreeningRoom
+import domain.ScreeningRoomName
+import domain.ScreeningSchedule
+import domain.Seat
+import domain.SeatPosition
+import domain.Seats
+import domain.TimeRange
+import domain.Title
+import java.time.LocalDate
+import java.time.LocalDateTime
+import java.time.LocalTime
 
 fun main() {
-    Controller().run()
+    Controller(
+        ScreeningSchedule(
+            screenings =
+                listOf(
+                    Screening(
+                        movie =
+                            Movie(
+                                title = Title("허닛"),
+                                runningTime = RunningTime(167),
+                                screeningPeriod =
+                                    ScreeningPeriod(
+                                        startDate = LocalDate.of(2026, 4, 8),
+                                        endDate = LocalDate.of(2026, 4, 9),
+                                    ),
+                            ),
+                        room =
+                            ScreeningRoom(
+                                name = ScreeningRoomName("커피"),
+                                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
+                                seats =
+                                    Seats(
+                                        listOf(
+                                            Seat(
+                                                position =
+                                                    SeatPosition(
+                                                        Row("A"),
+                                                        Column(1),
+                                                    ),
+                                            ),
+                                        ),
+                                    ),
+                            ),
+                        startTime = LocalDateTime.of(2026, 4, 8, 10, 0),
+                    ),
+                ),
+        ),
+    ).run()
 }
--- src/main/kotlin/controller/Controller.kt
+++ src/main/kotlin/controller/Controller.kt
@@ -1,68 +1,21 @@
 package controller
 
-import domain.Column
 import domain.Money
-import domain.Movie
 import domain.PaymentSystem
 import domain.PaymentType
 import domain.Point
-import domain.Row
-import domain.RunningTime
 import domain.Screening
-import domain.ScreeningPeriod
-import domain.ScreeningRoom
-import domain.ScreeningRoomName
 import domain.ScreeningSchedule
-import domain.Seat
-import domain.SeatPosition
-import domain.Seats
 import domain.Ticket
 import domain.TicketBucket
-import domain.TimeRange
 import domain.Title
 import view.InputView
 import view.OutputView
 import java.time.LocalDate
-import java.time.LocalDateTime
-import java.time.LocalTime
 
-class Controller {
-    val schedule =
-        ScreeningSchedule(
-            screenings =
-                listOf(
-                    Screening(
-                        movie =
-                            Movie(
-                                title = Title("허닛"),
-                                runningTime = RunningTime(167),
-                                screeningPeriod =
-                                    ScreeningPeriod(
-                                        startDate = LocalDate.of(2026, 4, 8),
-                                        endDate = LocalDate.of(2026, 4, 9),
-                                    ),
-                            ),
-                        room =
-                            ScreeningRoom(
-                                name = ScreeningRoomName("커피"),
-                                operatingTime = TimeRange(LocalTime.of(10, 0), LocalTime.of(18, 0)),
-                                seats =
-                                    Seats(
-                                        listOf(
-                                            Seat(
-                                                position =
-                                                    SeatPosition(
-                                                        Row("A"),
-                                                        Column(1),
-                                                    ),
-                                            ),
-                                        ),
-                                    ),
-                            ),
-                        startTime = LocalDateTime.of(2026, 4, 8, 10, 0),
-                    ),
-                ),
-        )
+class Controller(
+    val schedule: ScreeningSchedule,
+) {
     val paymentSystem = PaymentSystem()
 
     fun run() {
