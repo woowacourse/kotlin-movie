db28dbfa720904c75c7413d246a56e3d0eec811c
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-09 17:20:47 +0900

    fix: domain 수정에 따른 test 코드 수정

--- src/test/kotlin/movie/domain/payment/PriceCalculatorTest.kt
+++ src/test/kotlin/movie/domain/payment/PriceCalculatorTest.kt
@@ -5,43 +5,68 @@ import movie.domain.amount.Point
 import movie.domain.discount.DiscountPolicies
 import movie.domain.discount.MovieDayDiscount
 import movie.domain.discount.TimeDiscount
+import movie.domain.reservation.Reservation
+import movie.domain.reservation.Reservations
+import movie.domain.screening.Screen
+import movie.domain.screening.Screening
 import movie.domain.screening.ScreeningDateTime
+import movie.domain.seat.ReservatedSeats
+import movie.domain.seat.Seat
+import movie.domain.seat.SeatGrade
+import movie.domain.seat.Seats
+import movie.domain.seat.SelectedSeats
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import java.time.LocalDate
 import java.time.LocalTime
 
 class PriceCalculatorTest {
+
     @Test
     fun `할인 정책, 포인트 차감, 결제 수단 할인 순서로 결제를 진행한다`() {
         // given
-        val ticketPrice = Money(10000)
-        val discountPolicies =
+        val screening = Screening(
+            title = "토이 스토리",
+            screen = Screen(1, Seats.createDefault()),
+            screeningDateTime = ScreeningDateTime(
+                LocalDate.of(2026, 1, 10),
+                LocalTime.of(10, 0),
+                LocalTime.of(12, 0),
+            ),
+            reservatedSeats = ReservatedSeats(
+                listOf(
+                    Seat("C", 1, SeatGrade.S),
+                    Seat("C", 2, SeatGrade.S),
+                )
+            )
+        )
+
+        val selectedSeats = SelectedSeats(
+            listOf(
+                Seat("C", 1, SeatGrade.S),
+            )
+        )
+
+        val reservations = Reservations(
+            listOf(
+                Reservation(screening, selectedSeats)
+            )
+        )
+
+        val priceCalculator = PriceCalculator(
             DiscountPolicies(
                 listOf(MovieDayDiscount()),
                 listOf(TimeDiscount()),
             )
-        val point = Point(1000)
-        val screenDateTime =
-            ScreeningDateTime(
-                LocalDate.of(2026, 1, 10),
-                LocalTime.of(10, 0),
-                LocalTime.of(12, 0),
-            )
-        val priceCalculator =
-            PriceCalculator(
-                ticketPrice,
-                screenDateTime,
-                discountPolicies,
-                point,
-                CreditCard(),
-            )
+        )
 
-        // when
-        val result = priceCalculator.calculate()
+        val result = priceCalculator.calculate(
+            reservations,
+            Point(1000),
+            CreditCard(),
+        )
 
-        // then
-        assertThat(result.price).isEqualTo(Money(5700))
-        assertThat(result.usagePoint).isEqualTo(Point(1000))
+        assertThat(result.totalPrice).isEqualTo(Money(12540))
+        assertThat(result.usedPoint).isEqualTo(Point(1000))
     }
 }
