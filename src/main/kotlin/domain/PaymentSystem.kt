6142a299c45480c8fb01e813d8257b623b575b59
baekcii <bsy8470@naver.com>
2026-04-09 12:29:24 +0900

    feat: PaymentSystem 클래스 구현

---
+++ src/main/kotlin/domain/PaymentSystem.kt
@@ -0,0 +1,18 @@
+package domain
+
+class PaymentSystem(
+    private val discountPolicy: DiscountPolicy = DiscountPolicy()
+) {
+    fun calculate(
+        point: Point,
+        payment: PaymentType,
+        ticketBucket: TicketBucket
+    ): Money {
+        var total = Money(0)
+
+        ticketBucket.tickets.forEach { ticket ->
+            total += discountPolicy.calculateDiscountResult(ticket.totalPrice,point, ticket.screening.startTime, payment)
+        }
+        return total
+    }
+}
