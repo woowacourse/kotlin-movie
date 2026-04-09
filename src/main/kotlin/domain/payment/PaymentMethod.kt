bbfedf28224b02c9943b2e4ecf57819355c17020
gykim_kr <k_bright_st@pusan.ac.kr>
2026-04-09 17:12:09 +0900

    feat: PaymentMethod 내 validate 메서드 추가 및 테스트 로직 작성

--- src/main/kotlin/domain/payment/PaymentMethod.kt
+++ src/main/kotlin/domain/payment/PaymentMethod.kt
@@ -4,5 +4,15 @@ enum class PaymentMethod(val discountRate: Double) {
     CREDIT_CARD(0.05),
     CASH(0.02);
 
+    companion object {
+        fun validate(number: Int): PaymentMethod {
+            return when (number) {
+                1 -> CREDIT_CARD
+                2 -> CASH
+                else -> throw IllegalArgumentException("올바른 결제 수단을 선택해 주세요.")
+            }
+        }
+    }
+
     fun calculateDiscount(price: Int): Int = (price * (1 - discountRate)).toInt()
 }
