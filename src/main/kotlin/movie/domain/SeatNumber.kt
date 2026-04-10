7c904d93ba0529371b9a30909e7e067c40ec3877
cksckckcks <chan679999@naver.com>
2026-04-10 23:31:31 +0900

    feat: SeatNumber 도메인 구현

---
+++ src/main/kotlin/movie/domain/SeatNumber.kt
@@ -0,0 +1,13 @@
+package movie.domain
+
+data class SeatNumber(
+    val row: Char,
+    val col: Int,
+) {
+    init {
+        require(row in 'A'..'Z') { "좌석 번호의 행은 A부터 Z사이의 알파뱃이어야 합니다." }
+        require(col > 0) { "좌석 번호의 열은 0보다 큰 숫자여야 합니다." }
+    }
+
+    override fun toString(): String = "$row$col"
+}
