d506992c374fa1c2c3db904b1124d40f2822c7e0
cksckckcks <chan679999@naver.com>
2026-04-10 23:55:01 +0900

    feat: Price 도메인 구현

---
+++ src/main/kotlin/movie/domain/Price.kt
@@ -0,0 +1,16 @@
+package movie.domain
+
+@JvmInline
+value class Price(val amount: Int) {
+    init {
+        require(amount >= 0) { "가격은 0원 이상이어야 합니다." }
+    }
+
+    operator fun minus(target: Price): Price {
+        return Price(this.amount - target.amount)
+    }
+
+    operator fun plus(target: Price): Price {
+        return Price(this.amount + target.amount)
+    }
+}
