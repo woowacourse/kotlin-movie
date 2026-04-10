f9d26af88eeb105755b107becaccef8b41f2fd67
chohs4164 <josh100as@gmail.com>
2026-04-10 20:59:54 +0900

    feat: Theaters 도메인 내 극장 추가 기능 구현

--- src/main/kotlin/movie/domain/movie/Theaters.kt
+++ src/main/kotlin/movie/domain/movie/Theaters.kt
@@ -3,5 +3,11 @@ package movie.domain.movie
 class Theaters(
     theaters: List<Theater>,
 ) {
-    private val value = theaters.toList()
+    private val value = theaters.toMutableList()
+
+    fun addTheater(
+        newTheater: Theater
+    ) {
+        value.add(newTheater)
+    }
 }
