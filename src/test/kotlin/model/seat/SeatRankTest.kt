c7ddb1d8c46fe20c8fa49f5c3f4d71cb0730ff22
NoeyhOj <118670117+NoeyhOj@users.noreply.github.com>
2026-04-11 05:10:04 +0900

    test: SeatRank 기능 구현 및 README.md 업데이트

---
+++ src/test/kotlin/model/seat/SeatRankTest.kt
@@ -0,0 +1,17 @@
+package model.seat
+
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class SeatRankTest {
+    @Test
+    fun `각 등급은 12_000, 15_000, 18_000원의 가격을 갖는다`() {
+        val seatSRank = SeatRank.S_RANK
+        val seatARank = SeatRank.A_RANK
+        val seatBRank = SeatRank.B_RANK
+
+        assertThat(seatSRank.price).isEqualTo(18000)
+        assertThat(seatARank.price).isEqualTo(15000)
+        assertThat(seatBRank.price).isEqualTo(12000)
+    }
+}
