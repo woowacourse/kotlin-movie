9e60ccdb8b3603ad0620baf5e328f0a550733fd3
parkhyomi <phyomi3@naver.com>
2026-04-11 10:08:27 +0900

    feat: 영화 상영 시간 조회 기능 구현
    - App.kt: 입력받은 제목과 날짜를 기반으로 상영 시간 목록을 출력하는 로직 추가
    - CinemaController: 상영 목록에서 시작 시간만 추출하는 `findScreeningsTime` 메서드 추가

--- src/main/kotlin/App.kt
+++ src/main/kotlin/App.kt
@@ -1,22 +1,25 @@
 import controller.CinemaController
+import view.SeatLayoutView
 import java.time.LocalDate
 
 fun main() {
     val controller = CinemaController()
 
+    // 영화 제목을 입력 받고 해당 제목 영화의 상영 목록
     println("영화 제목 입력해")
     val title = readln()
-
     val screenings = controller.findScreeningTitle(title)
 
+    // 영화 날짜를 입력 받고 제목으로 필터링 된 리스트 중 해당 날짜 리스트 반환
     println("날짜 입력해")
     val date = readln().split("-").map { it.toInt() }
     val formattingDate = LocalDate.of(date[0], date[1], date[2])
+    val findTitleAndDateSchedule= controller.findScreeningsDate(screenings, formattingDate)
 
-    // 제목과 날짜가 동일한 스크린의 목록
-    controller.findScreeningsDate(screenings, formattingDate).forEach {
-        println("제목 : ${it.movie}")
-        println("영화 시작 시간 : ${it.startTime}")
-        println("영화 종료 시간 : ${it.endTime}")
+    // 영화 날짜를 입력 받고 제목으로 필터링 된 리스트 중 상영 시간들만 조회
+    println("해당 날짜의 상영 목록")
+    val findScheduleTimeList = controller.findScreeningsTime(findTitleAndDateSchedule)
+    findScheduleTimeList.forEachIndexed { index, time ->
+        println("[${index + 1}] $time")
     }
 }
--- src/main/kotlin/controller/CinemaController.kt
+++ src/main/kotlin/controller/CinemaController.kt
@@ -1,8 +1,8 @@
 package controller
 
 import domain.ScreeningSchedule
-import domain.ScreeningTemplate
 import domain.Seat
+import domain.defaultScreeningSeeds
 import domain.model.Movie
 import domain.model.screen.Screening
 import domain.model.seat.SeatAvailability
@@ -30,13 +30,25 @@ class CinemaController(
     ): Screening = screeningSchedule.createScreening(movieTitle, screeningDate, startTime)
 
     // 특정 제목의 영화 상영 목록을 조회한다.
-    fun findScreeningTitle(title: String): List<Screening> = screeningSchedule.screeningsOfMovieTitle(title)
+    fun findScreeningTitle(title: String): List<Screening> =
+        screeningSchedule.screeningsOfMovieTitle(title)
 
     // 특정 날짜의 특정 영화 상영 목록을 조회한다.
     fun findScreeningsDate(
         screening: List<Screening>,
         date: LocalDate,
     ): List<Screening> = screeningSchedule.screeningsOfMovieDate(screening, date)
+
+    // 상영 목록들 중에서 상영시간만 추출한다
+    fun findScreeningsTime(
+        screening: List<Screening>,
+    ): List<LocalTime> {
+        val timeList = mutableListOf<LocalTime>()
+        screening.forEach {
+            timeList.add(it.startTime)
+        }
+        return timeList
+    }
 
     // 특정 상영의 좌석 상태를 조회한다.
     fun findSeatStatuses(
@@ -53,73 +65,3 @@ class CinemaController(
         seats: List<Seat>,
     ): Screening = screeningSchedule.reserveSeats(movieTitle, date, startTime, seats)
 }
-
-private fun defaultScreeningSeeds(): List<ScreeningTemplate> =
-    // 상영 제목 , 상영 날짜, 상영 시작 시간 리스트들
-    listOf(
-        ScreeningTemplate(
-            movieTitle = "탑건: 매버릭",
-            screeningDate = LocalDate.of(2026, 4, 6),
-            startTime = LocalTime.of(10, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "마더",
-            screeningDate = LocalDate.of(2026, 4, 6),
-            startTime = LocalTime.of(13, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "아이언맨 3",
-            screeningDate = LocalDate.of(2026, 4, 6),
-            startTime = LocalTime.of(16, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "탑건: 매버릭",
-            screeningDate = LocalDate.of(2026, 4, 7),
-            startTime = LocalTime.of(10, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "스파이더맨: 노 웨이 홈",
-            screeningDate = LocalDate.of(2026, 4, 7),
-            startTime = LocalTime.of(13, 30),
-        ),
-        ScreeningTemplate(
-            movieTitle = "탑건: 매버릭",
-            screeningDate = LocalDate.of(2026, 4, 8),
-            startTime = LocalTime.of(10, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "남은 인생 10년",
-            screeningDate = LocalDate.of(2026, 4, 8),
-            startTime = LocalTime.of(14, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "오늘 밤 이세상에서 사랑이 사라진다 해도",
-            screeningDate = LocalDate.of(2026, 4, 9),
-            startTime = LocalTime.of(12, 20),
-        ),
-        ScreeningTemplate(
-            movieTitle = "아이언맨 3",
-            screeningDate = LocalDate.of(2026, 4, 10),
-            startTime = LocalTime.of(9, 50),
-        ),
-        ScreeningTemplate(
-            movieTitle = "체인소맨",
-            screeningDate = LocalDate.of(2026, 4, 10),
-            startTime = LocalTime.of(16, 0),
-        ),
-        ScreeningTemplate(
-            movieTitle = "호퍼스",
-            screeningDate = LocalDate.of(2026, 4, 11),
-            startTime = LocalTime.of(20, 10),
-        ),
-        ScreeningTemplate(
-            movieTitle = "스파이더맨: 노 웨이 홈",
-            screeningDate = LocalDate.of(2026, 4, 12),
-            startTime = LocalTime.of(15, 40),
-        ),
-        ScreeningTemplate(
-            movieTitle = "마더",
-            screeningDate = LocalDate.of(2026, 4, 13),
-            startTime = LocalTime.of(11, 0),
-        ),
-    )
