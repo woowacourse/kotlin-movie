50b2f0a86d1780704b61cd2297f75fc86737e9ae
byunghyunkim0 <bb05qudgus@gmail.com>
2026-04-11 17:04:38 +0900

    feat: ReservedSeats 도메인 코드 작성

--- src/main/kotlin/domain/timetable/items/ReservedSeats.kt
+++ src/main/kotlin/domain/timetable/items/ReservedSeats.kt
@@ -1,15 +1,21 @@
 package domain.timetable.items
 
+import domain.money.Money
 import domain.seat.Seat
 
 class ReservedSeats(
     private val reservedSeats: MutableList<Seat> = mutableListOf(),
 ) {
     fun addSeat(seat: Seat) {
-        require(isReserved(seat)) { "이미 예약된 좌석 입니다." }
+        require(!isReserved(seat)) { "이미 예약된 좌석 입니다." }
+        reservedSeats.add(seat)
     }
 
-    fun sumPrice() {
+    fun sumPrice(): Money {
+        val initMoney = Money(0)
+        return reservedSeats.fold(initMoney) { total, seat ->
+            seat.addSeatPrice(total)
+        }
     }
 
     private fun isReserved(seat: Seat): Boolean = reservedSeats.any { it.isExistSeat(seat) }
