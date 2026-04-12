d6a4956016707046caea95a00a4090f7f4227d9f
baekcii <bsy8470@naver.com>
2026-04-12 21:40:02 +0900

    test: 할인 정책 분리에 따른 테스트 코드 수정

--- src/test/kotlin/domain/DiscountPolicyTest.kt
+++ src/test/kotlin/domain/DiscountPolicyTest.kt
@@ -1,48 +1,57 @@
 package domain
 
+import domain.discount.MovieDayEvent
+import domain.discount.PaymentDiscount
+import domain.discount.TheaterEventDiscount
+import domain.discount.TimeEvent
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import java.time.LocalDate
-import java.time.LocalTime
+import java.time.LocalDateTime
 
 class DiscountPolicyTest {
+    private val paymentDiscountPolicy = PaymentDiscount()
+    private val movieDayEvent = MovieDayEvent()
+    private val timeEvent = TimeEvent()
+
+    private val eventDiscountPolicy = TheaterEventDiscount()
+
     @Test
     fun `카드 결제 시 할인 금액을 반환한다`() {
         val given = Money(10000)
-        val result = DiscountPolicy().paymentDiscount(given, PaymentType.CREDIT_CARD)
+        val result = paymentDiscountPolicy.discount(given, PaymentType.CREDIT_CARD)
 
-        assertThat(result).isEqualTo(Money(500))
+        assertThat(result).isEqualTo(Money(9500))
     }
 
     @Test
     fun `현금 결제 시 할인 금액을 반환한다`() {
         val given = Money(10000)
-        val result = DiscountPolicy().paymentDiscount(given, PaymentType.CASH)
+        val result = paymentDiscountPolicy.discount(given, PaymentType.CASH)
 
-        assertThat(result).isEqualTo(Money(200))
+        assertThat(result).isEqualTo(Money(9800))
     }
 
     @Test
     fun `무비데이 조건에 해당하는 경우 할인 금액을 반환한다`() {
         val given = Money(10000)
-        val result =
-            DiscountPolicy().movieDayDiscount(
-                money = given,
-                date = LocalDate.of(2026, 4, 10),
-            )
+        val result = movieDayEvent.discount(given, LocalDateTime.of(2026, 4, 10, 10, 0))
 
-        assertThat(result).isEqualTo(Money(1000))
+        assertThat(result).isEqualTo(Money(9000))
     }
 
     @Test
     fun `특정 시간대 조건에 해당하는 경우 할인 금액을 반환한다`() {
         val given = Money(10000)
-        val result =
-            DiscountPolicy().timeDiscount(
-                money = given,
-                time = LocalTime.of(10, 0),
-            )
+        val result = timeEvent.discount(given, LocalDateTime.of(2026, 4, 10, 10, 0))
 
-        assertThat(result).isEqualTo(Money(2000))
+        assertThat(result).isEqualTo(Money(8000))
+    }
+
+    @Test
+    fun `이벤트 할인을 순서대로 적용하여 할인 금액을 반환한다`() {
+        val given = Money(10000)
+        val result = eventDiscountPolicy.discount(given, LocalDateTime.of(2026, 4, 10, 10, 0))
+
+        assertThat(result).isEqualTo(Money(7000))
     }
 }
