08f04b2c3c755319365c1f3e1f70f382eaa00004
조은경 <108224432+eungyeong12@users.noreply.github.com>
2026-04-12 23:56:03 +0900

    fix: 방어적 복사를 하도록 수정

--- src/main/kotlin/movie/domain/screening/Screenings.kt
+++ src/main/kotlin/movie/domain/screening/Screenings.kt
@@ -2,9 +2,11 @@ package movie.domain.screening
 
 import java.time.LocalDate
 
-data class Screenings(
-    private val screenings: List<Screening>,
+class Screenings(
+    screenings: List<Screening>,
 ) {
+    private val screenings = screenings.toList()
+
     fun hasScreeningOnDate(date: LocalDate): Boolean = screenings.any { it.screeningDateTime.date == date }
 
     fun getScreeningsByDate(date: LocalDate): List<Screening> = screenings.filter { it.screeningDateTime.date == date }
