c18a99a0909167c2d5de20559c6630acd06e8ed4
joon0447 <joon0447@naver.com>
2026-04-12 14:06:27 +0900

    refactor: movieScreenings private으로 변경

--- src/main/kotlin/model/schedule/ScreenSchedule.kt
+++ src/main/kotlin/model/schedule/ScreenSchedule.kt
@@ -7,10 +7,8 @@ import model.movie.MovieId
 class ScreenSchedule(
     private val screenId: String,
     private val servicePeriod: CinemaTimeRange,
-    movieScreenings: List<MovieScreening>,
+    private val movieScreenings: List<MovieScreening>,
 ) {
-    private val movieScreenings = movieScreenings.toList()
-
     init {
         require(movieScreenings.all { isContainServicePeriod(it.screenTime.start) }) { "상영관의 운영 시작 시간보다 일찍 영화를 배정할 수 없습니다." }
         require(movieScreenings.all { isContainServicePeriod(it.screenTime.end) }) { "상영관의 운영 종료 시간보다 늦게 영화를 배정할 수 없습니다." }
