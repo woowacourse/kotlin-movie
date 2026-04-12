a9c5a7184b8f7f197bda79ebd63c1a5e4b4e4b0f
cbnuLeehyunwoo <henow123@naver.com>
2026-04-13 02:07:29 +0900

    fix: 3개 이상의 티켓부터 overlapping 검증이 되지 않던 문제 수정

--- src/main/kotlin/domain/ticket/TicketBucket.kt
+++ src/main/kotlin/domain/ticket/TicketBucket.kt
@@ -8,7 +8,7 @@ data class TicketBucket(
 
     fun addTicket(newTicket: Ticket): TicketBucket {
         for (ticket in tickets) {
-            if (!ticket.isOverlapping(newTicket)) break
+            if (!ticket.isOverlapping(newTicket)) continue
 
             require(ticket.isSameScreening(newTicket)) { "동일한 시간대의 영화는 예매할 수 없습니다." }
             require((ticket.hasSameSeat(newTicket)).not()) { "이미 선택하신 좌석입니다." }
