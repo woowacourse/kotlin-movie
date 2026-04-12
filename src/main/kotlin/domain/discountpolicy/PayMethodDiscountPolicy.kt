37baa8c3fb8f96ae1175b4f04f2aaea8719bc601
byunghyunkim0 <bb05qudgus@gmail.com>
2026-04-12 13:28:51 +0900

    test: PayMethodDiscountPolicy test 코드 작성
    - PayMethodDiscountPolicy 인터페이스 구현

--- src/main/kotlin/domain/discountpolicy/PayMethodDiscountPolicy.kt
+++ src/main/kotlin/domain/discountpolicy/PayMethodDiscountPolicy.kt
@@ -1,3 +1,42 @@
 package domain.discountpolicy
 
-interface PayMethodDiscountPolicy
+import domain.money.Money
+
+interface PayMethodDiscountPolicy {
+    fun applyDiscount(
+        price: Money,
+        payMethod: PayMethod,
+    ): Money
+}
+
+class CardDiscountPolicy(
+    private val payMethodDiscountCondition: PayMethodDiscountCondition,
+) : PayMethodDiscountPolicy {
+    override fun applyDiscount(
+        price: Money,
+        payMethod: PayMethod,
+    ): Money {
+        if (payMethodDiscountCondition.isSatisfiedBy(payMethod)) return price * CARD_DISCOUNT
+        return price
+    }
+
+    companion object {
+        private const val CARD_DISCOUNT = 0.95
+    }
+}
+
+class CashDiscountPolicy(
+    private val payMethodDiscountCondition: PayMethodDiscountCondition,
+) : PayMethodDiscountPolicy {
+    override fun applyDiscount(
+        price: Money,
+        payMethod: PayMethod,
+    ): Money {
+        if (payMethodDiscountCondition.isSatisfiedBy(payMethod)) return price * CASH_DISCOUNT
+        return price
+    }
+
+    companion object {
+        private const val CASH_DISCOUNT = 0.98
+    }
+}
